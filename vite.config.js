e184d08392fd68fbae99c693383bfada5e72f6cd
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-12-18 20:54:36 +0100

    test 11

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig(({ command }) => {
           entryFileNames: 'commonHelpers.js',
         },
       },
-      outDir: './dist',
+      outDir: '../dist',
     },
     plugins: [
       injectHTML(),

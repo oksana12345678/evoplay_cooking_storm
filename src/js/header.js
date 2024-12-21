ac8e4c096bbe0b6aa0f85c64c26388f4f2894280
d-sofiika <soniadrozd2709@gmail.com>
2024-12-21 15:45:50 +0200

    modal-menu

--- src/js/header.js
+++ src/js/header.js
@@ -1,22 +1,22 @@
 const btnOpen = document.querySelector('.btn-burger');
 const btnClose = document.querySelector('.close-btn');
 const modal = document.querySelector('.modal-overlay');
+const modalContent = document.querySelector('.modal-content');
 const burgerMenu = document.querySelector('.burger-menu');
+const navigation = document.querySelectorAll('.menu-item-modal');
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
 
-      const targetId = this.getAttribute('href').substring(1); // Отримуємо id елемента без "#"
+      const targetId = this.getAttribute('href').substring(1);
       const target = document.getElementById(targetId);
 
       if (target) {
-        // Рахуємо відстань до цілі
-        const offset = 90; // Відступ для видимості заголовка
+        const offset = 90;
         const targetPosition = target.offsetTop - offset;
 
-        // Плавна прокрутка
         window.scrollTo({
           top: targetPosition,
           behavior: 'smooth',
@@ -25,7 +25,25 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
 btnOpen.addEventListener('click', () => {
   burgerMenu.classList.toggle('is-open');
   modal.classList.toggle('is-open');
+});
+modalContent.addEventListener('click', e => {
+  e.stopPropagation();
+});
+
+modal.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.classList.remove('is-open');
+    burgerMenu.classList.remove('is-open');
+  }
+});
+
+navigation.forEach(item => {
+  item.addEventListener('click', () => {
+    modal.classList.remove('is-open');
+    burgerMenu.classList.remove('is-open');
+  });
 });
